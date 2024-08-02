32d1f7ee91fb3b9d2369a7192ec84932db457492
Dasher <55836007+the1andonlyDasher@users.noreply.github.com>
2024-08-02 10:22:05 +0200

    changed bgColors

---
+++ src/ts/bgColors.tsx
@@ -0,0 +1,62 @@
+// diese Farben sind für die jeweiligen sections
+export const targetColors: any = {
+    landing: ["#c5cf8f", "#89B069", "#699051"],
+    landingBusiness: ["#ccf1fa", "#97b9c1", "#6790a5"],
+    business: ["#e3f9ff", "#fffde1", "#fbffe1"],
+    "/": ["#ffffff", "#fffde1", "#f6ffe1"],
+    science: ["#ffffff", "#fffffe", "#fffffa"],
+    slider: ["#d813f2", "#fffffe", "#a9a940"],
+    newSection: ["#d813f2", "#fffffe", "#a9a940"],
+    symbols: ["#ffffff", "#fffffe", "#fffffa"],
+    perspective: ["#ffffff", "#fffffe", "#fffffa"],
+    braincare: ["#ffffff", "#fffffe", "#fffffa"],
+    universal: ["#ffffff", "#fffffe", "#fffffa"],
+    architekt: ["#ffffff", "#fffffe", "#fffffa"],
+    freund: ["#ffffff", "#fffffe", "#fffffa"],
+    head: ["#ffffff", "#fffffe", "#fffffa"],
+    services: ["#ffffff", "#fffffe", "#fffffa"],
+    faq: ["#ffffff", "#fffffe", "#fffffa"],
+    different: ["#ffffff", "#fffffe", "#fffffa"],
+    mountain: ["#ffffff", "#fffffe", "#fffffa"],
+}
+
+// diese Farben ändern den ersten teil des Navigationstitels
+export const targetColorsNavTitle: any = {
+    landing: "#ffffff",
+    landingBusiness: "#32689C",
+    business: "#32689C",
+    "/": "#ffffff",
+    science: "#506c00",
+    slider: "#006d8b",
+    symbols: "#506c00",
+    perspective: "#506c00",
+    braincare: "#506c00",
+    universal: "#506c00",
+    architekt: "#506c00",
+    freund: "#506c00",
+    head: "#506c00",
+    services: "#506c00",
+    faq: "#506c00",
+    different: "#506c00",
+    mountain: "#506c00",
+}
+
+// diese Farben ändern den zweiten Teil des Navigationstitels (also Business, Dashboard etc)
+export const addColors: any = {
+    landing: "#ffffff",
+    landingBusiness: "#B38224",
+    "/": "#ffffff",
+    business: "#B38224",
+    science: "#7e9b2e",
+    symbols: "#506c00",
+    perspective: "#506c00",
+    braincare: "#506c00",
+    universal: "#506c00",
+    architekt: "#506c00",
+    freund: "#506c00",
+    head: "#506c00",
+    services: "#506c00",
+    faq: "#506c00",
+    different: "#506c00",
+    mountain: "#506c00",
+}
