694ae8b21ee466a27fd47266b8c91c5b1763640e
Dasher <55836007+the1andonlyDasher@users.noreply.github.com>
2024-08-20 09:21:44 +0200

    linting

--- src/ts/GL.tsx
+++ src/ts/GL.tsx
@@ -207,7 +207,7 @@ const GL = (props: glProps) => {
 
 
     return (<>
-        {/* {!shaderCompiled && <Loader />} */}
+        {!shaderCompiled && <Loader />}
         <div className="canvas__wrapper">
 
             <Canvas
